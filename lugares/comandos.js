8c1e19bf93dfec8e31a876a0e739d7eae17b6be5
Matias Bensan A <mbensan@gmail.com>
2022-08-10 12:20:26 -0400

    Uso de comandos usando process.argv para pasar parámetros

--- lugares/comandos.js
+++ lugares/comandos.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg')
+const get_distancia = require('./funciones.js')
 
 const config = {
   user: 'postgres',
@@ -15,7 +16,7 @@ client.connect(err => {
   }
 })
 
-
+// Funciones para manejar los lugares
 async function nuevoLugar (nombre, lat, long) {
 
 
@@ -25,9 +26,49 @@ async function nuevoLugar (nombre, lat, long) {
   client.end()
 }
 
-nuevoLugar('Parque Nacional Conguillío', -38.69095, -71.67178)
+async function mostrarLugares () {
+  const respuesta = await client.query('select * from lugares')
+  console.log(respuesta.rows);
+  client.end()
+}
 
-// Ejercicio:
-// 1. Agregar por lo menos 3 lugares
-// 2. Buscar un lugar en base a su id, y mostrarlo en consola
+async function mostrarDistancia(id_lugar1, id_lugar2) {
+  let lugar1 = await client.query(`select * from lugares where id=${id_lugar1}`)
+  lugar1 = lugar1.rows[0]
 
+  let lugar2 = await client.query(`select * from lugares where id=${id_lugar2}`)
+  lugar2 = lugar2.rows[0]
+
+  let distancia = get_distancia(lugar1.lat, lugar1.long, lugar2.lat, lugar2.long)
+  distancia = Math.round(distancia)
+  
+  console.log(`La distancia entre ${lugar1.nombre} y ${lugar2.nombre} es de ${distancia} Kms.`);
+  client.end()
+}
+
+
+// Acciones 
+//mostrarLugares()
+const accion = process.argv[2]
+
+if (accion == 'mostrar') {
+  mostrarLugares()
+}
+else if (accion == 'crear') {
+
+  const nombre = process.argv[3]
+  const lat = process.argv[4]
+  const long = process.argv[5]
+
+  nuevoLugar(nombre, lat, long)
+}
+else if (accion == 'distancia') {
+
+  const id_lugar1 = process.argv[3]
+  const id_lugar2 = process.argv[4]
+
+  mostrarDistancia(id_lugar1, id_lugar2)
+}
+else {
+  console.log(`Acción ${accion} no implementada`);
+}
